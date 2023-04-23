8438b312c21bcb5e87256af4bfc340d2610c3aee
Rakantor <rakantor.dev@gmail.com>
2023-04-23 23:33:12 +0200

    Add ImageCarousel.vue

---
+++ components/ImageCarousel.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-dialog v-model="show" class="d-flex align-center justify-center">
+    <v-carousel
+      :model-value="index"
+      :show-arrows="images.length > 1"
+      hide-delimiters
+    >
+      <v-carousel-item v-for="image of images" :key="image"
+        :src="image"
+        class="ma-auto"
+      ></v-carousel-item>
+    </v-carousel>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'ImageCarousel',
+  data: () => ({
+    show: false,
+    images: [],
+    index: 0
+  })
+}
+</script>
