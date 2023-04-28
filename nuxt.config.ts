ab75126998f7cd85edf410cdb8623c1520d097d1
Rakantor <rakantor.dev@gmail.com>
2023-04-28 19:21:20 +0200

    Add i18n messages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -72,9 +72,10 @@ export default defineNuxtConfig({
             Eager to dive deeper into the world of coding, I attended a {htl}\
             specializing in IT in 2009, where I learned C, Java, HTML and CSS.\
             Some years later, I pursued a degree in {bi} with a specialization\
-            in Web and App development.',
+            in Web and App development. I have acquired the majority of my knowledge through self-teaching\
+            while working on personal projects over the past ~15 years.',
           bioTitle: 'My Journey as a Developer',
-          bioSubtitle: 'Here\'s some of the tech that I\'ve used and worked with before:',
+          bioSubtitle: 'Here\'s some of the tech that I\'ve worked with before:',
           getInTouch: 'Get in touch',
           greeting: 'Hey. I\'m Manuel.',
           headerAbout: 'About',
@@ -84,7 +85,7 @@ export default defineNuxtConfig({
           introduction: 'I\'m a Software Developer based in Vienna, Austria.\
             I develop various types of applications.\
             Explore my {portfolio} to view a showcase of my projects.',
-          imprint: 'Imprint & Privacy',
+          imprint: 'Imprint',
           iuGamerApp: 'An Android app designed to help a group of board game enthusiasts\
             better organize their regular evening game sessions.\
             Users will be reliably informed about the time and location of the next appointment.\
@@ -105,7 +106,12 @@ export default defineNuxtConfig({
           portfolio: 'portfolio',
           portfolioTitle: 'Some of my projects',
           toolsPlatforms: 'Tools & Platforms',
-          toriiJava: 'A Japanese vocabulary learning tool that harnesses the power of spaced repetition to make memorizing new words a breeze.',
+          toriiJava: 'A Japanese vocabulary learning tool that harnesses the power of spaced repetition\
+            to make memorizing new words a breeze. It combines a straightforward interface with robust features.\
+            Designed for both casual learners and those preparing for the Japanese Language Proficiency Test (JLPT),\
+            Torii offers specialized vocabulary lists and review methods.\
+            Key features include audio reviews, font randomization, progress tracking,\
+            and automatic cloud synchronization.',
           toriiWeb: 'Version 2 of my Japanese vocabulary learning tool brings significant enhancements\
             over its predecessor. New features include offline functionality (thanks to utilizing IndexedDB),\
             an integrated dictionary for single-click word addition,\
@@ -115,20 +121,13 @@ export default defineNuxtConfig({
         },
         de: {
           bi: 'WI', // Wirtschaftsinformatik
-          bioBody: 'My interest in programming was sparked in 2007 when I began tinkering with SQL\
-            in an attempt to setup a private server for my favorite {mmorpg} (it was a success!)\
-            Eager to dive deeper into the world of coding, I attended a {htl}\
-            specializing in IT in 2009, where I learned C, Java, HTML and CSS.\
-            Some years later, I pursued a degree in {bi} with a specialization\
-            in Web and App development.',
-          // TODO
-          /* bioBody: 'Fasziniert schon als Kind. \
-            so richtig los gings für mich 2007 als ich mich mit SQL beschäftigt habe,\
-            in dem Versuch einen Privatserver für mein Lieblings-{mmorpg} aufzusetzen (erfolgreich!)\
-            Eager to dive deeper into the world of coding, eine auf IT spezialisierte {htl} zu besuchen\
-            specializing in IT in 2009, wo ich C, Java, HTML and CSS gelernt habe.\
-            Ein paar Jahre später habe ich ein {bi}-Studium absolviert with a specialization\
-            in Web and App development.', */
+          bioBody: 'Den Einstieg in die Programmierung habe ich 2007 gemacht, als ich mich mit SQL beschäftigt habe,\
+            in dem Versuch, einen Privatserver für mein Lieblings-{mmorpg} aufzusetzen (erfolgreich!)\
+            Nach dem Gymnasium habe ich mich 2009 dazu entschlossen, eine auf IT spezialisierte {htl} zu besuchen,\
+            wo ich die Grundlagen in C, Java, HTML and CSS gelernt habe.\
+            Ein paar Jahre später habe ich {bi} studiert - Wahlpflichtfächer Web- und App-Development.\
+            Den Großteil meines Wissens habe ich mir im Zuge persönlicher Projekte in den letzten ~15 Jahren\
+            autodidaktisch angeeignet.',
           bioTitle: 'Mein Werdegang als Developer',
           bioSubtitle: 'Einige der Technologien, mit denen ich gearbeitet habe:',
           getInTouch: 'Kontaktieren',
@@ -138,9 +137,9 @@ export default defineNuxtConfig({
           headerHome: 'Home',
           headerWork: 'Projekte',
           introduction: 'Ich bin ein Software Developer aus Wien, Österreich.\
-            Ich entwickle alle möglichen Applikationen.\
-            In meinem {portfolio} findest du eine Übersicht meiner Projekte.',
-          imprint: 'Impressum & Datenschutz',
+            Ich entwickle ganz unterschiedliche Applikationen.\
+            Die wichtigsten Projekte habe ich in meinem {portfolio} aufgelistet.',
+          imprint: 'Impressum',
           iuGamerApp: `Eine Android App, die einer Gruppe von Brettspielfans dabei helfen soll,\
             ihre regelmäßigen abendlichen Spieltermine besser zu organisieren.\
             Benutzer werden zuverlässig über Zeit & Ort des nächsten Termins informiert.\
@@ -161,12 +160,19 @@ export default defineNuxtConfig({
           portfolio: 'Portfolio',
           portfolioTitle: 'Ausgewählte Projekte',
           toolsPlatforms: 'Tools & Plattformen',
-          toriiJava: '[Beschreibung in Arbeit]',
+          toriiJava: 'Eine japanische Vokabeltrainer-App, die die Spaced Repetition Lernmethode nutzt, um\
+            den Lernprozess neuer Wörter so effektiv und effizient wie möglich zu gestalten.\
+            Die App verbindet eine simple Benutzeroberfläche mit leistungsstarken Funktionen.\
+            Torii eignet sich dank spezialisierter Vokabellisten und verschiedenen Wiederholungsmethoden\
+            sowohl für Casual-Learners als auch zur Vorbereitung auf den\
+            Japanese Language Proficiency Test (JLPT).\
+            Zu den wichtigsten Funktionen zählen Audio-Reviews, Font-Randomization, Progress-Tracking,\
+            und automatische Cloud-Synchronisation.',
           toriiWeb: `Version 2 meiner Vokabeltrainer-App bringt bedeutende Verbesserungen\
             gegenüber seinem Vorgänger. Neue Funktionen umfassen Offline-Funktionalität (dank der Nutzung von IndexedDB),\
             ein integriertes Wörterbuch für das Hinzufügen neuer Wörter mit nur einem Klick,\
             zusätzliche Überprüfungsmethoden für gelernte Vokabeln,\
-            ein Vorschau-Diagramm zur Planung von Lernsitzungen sowie\
+            ein Vorschau-Diagramm zur Planung von Lernsessions sowie\
             verbesserte Wortsuche und Statistiken.`
         }
       }
